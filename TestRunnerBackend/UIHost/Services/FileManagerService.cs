9c816bc8fd385761054bc84831b950adc2f6674e
Kirill Lebedev <nypifok@gmail.com>
2025-04-12 23:17:29 +0400

    Grpc basic setup for backend

---
+++ TestRunnerBackend/UIHost/Services/FileManagerService.cs
@@ -0,0 +1,22 @@
+using Grpc.Core;
+using TestRunner.Contract.Grpc.V1;
+
+namespace UIHost.Services;
+
+public class FileManagerService:FileManager.FileManagerBase
+{
+    public override Task<FileSystemTreeResponse> ChangeDirectory(ChangeDirectoryRequest request, ServerCallContext context)
+    {
+        return base.ChangeDirectory(request, context);
+    }
+
+    public override Task<ChooseTargetDirectoryResponse> ChooseTargetDirectory(ChooseTargetDirectoryRequest request, ServerCallContext context)
+    {
+        return base.ChooseTargetDirectory(request, context);
+    }
+
+    public override Task<FileSystemTreeResponse> OpenFileSystem(OpenFileSystemRequest request, ServerCallContext context)
+    {
+        return base.OpenFileSystem(request, context);
+    }
+}
