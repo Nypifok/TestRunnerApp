989d14de10ec17d13693ec8869031fd67d0876fe
Kirill Lebedev <nypifok@gmail.com>
2025-04-30 13:18:31 +0400

    Backend sync setup

--- TestRunnerBackend/src/TestRunner.UIHost/Program.cs
+++ TestRunnerBackend/src/TestRunner.UIHost/Program.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Net;
 using System.Net.Sockets;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -10,10 +11,15 @@ internal class Program
 {
     public static async Task Main(string[] args)
     {
+        var parentPid = ParseParentPid(args);
+        if (parentPid != null)
+        {
+            StartParentWatcher(parentPid.Value);
+        }
+        
         Log.Logger = new LoggerConfiguration()
             .Enrich.FromLogContext()
             .MinimumLevel.Verbose()
-            .WriteTo.Console()
             .WriteTo.File(Path.Combine(AppContext.BaseDirectory, "Logs", "log.txt"),
                 rollingInterval: RollingInterval.Day, rollOnFileSizeLimit: true)
             .CreateLogger();
@@ -30,7 +36,7 @@ internal class Program
 
 
         int port = Convert.ToUInt16(builder.Configuration["port"] ?? "5128");
-
+        
         
         builder.WebHost.ConfigureKestrel(options =>
         {
@@ -46,6 +52,37 @@ internal class Program
         //Registers all required Grpc services 
         app.UseGrpcServices();
 
-        app.Run();
+        app.Lifetime.ApplicationStarted.Register(() =>
+        {
+            Console.WriteLine("UIHOST_READY");
+        });
+        
+        await app.RunAsync();
+    }
+    private static int? ParseParentPid(string[] args)
+    {
+        for (int i = 0; i < args.Length - 1; i++)
+        {
+            if (args[i] == "--parent" && int.TryParse(args[i + 1], out var pid))
+                return pid;
+        }
+        return null;
+    }
+
+    private static void StartParentWatcher(int parentPid)
+    {
+        try
+        {
+            var parent = Process.GetProcessById(parentPid);
+            parent.EnableRaisingEvents = true;
+            parent.Exited += (s, e) =>
+            {
+                Environment.Exit(0);
+            };
+        }
+        catch (Exception)
+        {
+            Environment.Exit(0);
+        }
     }
 }
